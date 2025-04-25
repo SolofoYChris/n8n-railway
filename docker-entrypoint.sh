e9c4fe36eff6ad4a7574f45e0277f1433be163c2
o.surovtsev <o.surovtsev@spd-ukraine.com>
2025-04-25 23:08:13 +0300

    Simplify docker-entrypoint script by removing setup logic
    Removed redundant permission fixes and symbolic link creation logic from the script. These operations are assumed to be handled elsewhere or no longer necessary, streamlining the startup process.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,2 @@
 #!/bin/sh
-
-# Исправляем права на папку /home/node/.n8n если они неправильные
-if [ -d /home/node/.n8n ]; then
-    echo "Fixing permissions for /home/node/.n8n"
-    chown -R node:node /home/node/.n8n
-fi
-
-# Если нужен симлинк для второго mount path
-if [ ! -d /data ]; then
-    ln -s /home/node/.n8n /data
-fi
-
-# Запускаем n8n
 exec n8n
